0213b234ab8696f15d50c420b2759ee469a889d8
Aakash <editsky597@gmail.com>
2025-11-02 18:58:40 +0530

    added ratelimiter & helmet in socket

--- backend/templates/express-socket/src/app.js
+++ backend/templates/express-socket/src/app.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import helmet from 'helmet';
 import { errorHandler, notFound } from './middleware/error.middleware.js';
+import { rateLimiter } from './middleware/rateLimiter.js';
 
 
 // instance 
@@ -12,14 +14,17 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+app.use(helmet());
 
-// routes
+// health check route with rate limiting
+app.get("/health", rateLimiter(20), (req, res) => {
+    res.status(200).json({ status: "OK", message: "ALL IS WELL😂..." });
+});
 
 // default route
 app.get("/", (req, res) => {
     res.send("API is running...");
 });
-
 
 // error handling middlewares
 app.use(notFound);
