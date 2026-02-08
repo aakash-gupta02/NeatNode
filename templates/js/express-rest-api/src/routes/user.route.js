a50b7f54e90828f8a7edd498393082b85f6b3406
Aakash <editsky597@gmail.com>
2026-02-08 19:18:06 +0530

    added seprate validator for body, query & params

--- templates/js/express-rest-api/src/routes/user.route.js
+++ templates/js/express-rest-api/src/routes/user.route.js
@@ -1,14 +1,14 @@
 import express from "express";
 import { getUser, registerUser, loginUser } from "../controllers/user.controller.js";
 import { createUserSchema, loginSchema } from "../schemas/user.schema.js";
-import validate from "../middleware/validateRequest.middleware.js";
 import { protect } from "../middleware/auth.middleware.js";
+import { validateBody } from "../middleware/validateRequest.middleware.js";
 
 const router = express.Router();
 
 router.get("/me", protect, getUser);
-router.post("/register", validate(createUserSchema), registerUser);
-router.post("/login", validate(loginSchema), loginUser);
+router.post("/register", validateBody(createUserSchema), registerUser);
+router.post("/login", validateBody(loginSchema), loginUser);
 
 
 export default router;
--- templates/js/express-socket/src/middleware/validateRequest.middleware.js
+++ templates/js/express-socket/src/middleware/validateRequest.middleware.js
@@ -1,15 +1,28 @@
-// Generic middleware to validate req.body against a Joi schema
-const validate = (schema) => (req, res, next) => {
-  const { error } = schema.validate(req.body, { abortEarly: false });
+import { StatusCodes } from "http-status-codes";
+
+const validatePart = (part, schema) => (req, res, next) => {
+  const { error, value } = schema.validate(req[part], {
+    abortEarly: false,
+  });
 
   if (error) {
-    return res.status(400).json({
+    return res.status(StatusCodes.BAD_REQUEST).json({
       success: false,
       errors: error.details.map((err) => err.message),
     });
   }
 
+  // SAFE assignment rules
+  if (part === "query") {
+    Object.assign(req.query, value);
+  } else {
+    req[part] = value;
+  }
+
+
   next();
 };
 
-export default validate;
+export const validateBody = (schema) => validatePart("body", schema);
+export const validateParams = (schema) => validatePart("params", schema);
+export const validateQuery = (schema) => validatePart("query", schema);
